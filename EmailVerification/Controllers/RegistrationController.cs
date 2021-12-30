8fef98f16b01b039dbcf9d83aac0c337e90cd448
TajbiurShahriorRimon <tajbiurshahriorrimon@gmail.com>
2021-12-30 13:32:32 +0600

    first push

---
+++ EmailVerification/Controllers/RegistrationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmailVerification.Models;
+
+namespace EmailVerification.Controllers
+{
+    public class RegistrationController : Controller
+    {
+
+        UserInfoEntities db = new UserInfoEntities();
+
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult SaveData(UserSite model)
+        {
+            this.db.UserSites.Add(model);
+            db.SaveChanges();
+
+            return Json("Registration Successful", JsonRequestBehavior.AllowGet);
+        }
+    }    
+}
